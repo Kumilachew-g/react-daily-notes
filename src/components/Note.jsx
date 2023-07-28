10d4b1fe2b3969dc656f84d5c4e6a78153b38adc
Kumilachew-g <kumilachew1212@gmail.com>
2023-07-28 14:36:08 +0300

    Add more styling for the note component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,7 +2,14 @@ import { Paper } from '@material-ui/core';
 
 function Note() {
   return (
-    <Paper>
+    <Paper
+      style={{
+        minWidth: '200px',
+        minHeight: '100px',
+        padding: '10px',
+        margin: '20px',
+      }}
+    >
       <h1>Title</h1>
       <p>Content</p>
     </Paper>
