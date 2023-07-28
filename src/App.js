2ad5e3d2af957934464e0187b39283573a29a4bc
Kumilachew-g <kumilachew1212@gmail.com>
2023-07-28 17:13:14 +0300

    Create useState and implement it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import Footer from './components/Footer';
 import FormArea from './components/FormArea';
 import Header from './components/Header';
 import Note from './components/Note';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  console.log(notes);
+
+  function addNote(note) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  }
   return (
     <div>
       <Header />
-      <FormArea />
+      <FormArea addNote={addNote} />
       <Note />
       <Note />
       <Note />
