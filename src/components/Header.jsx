8b5d9d94ccf0ca194ebbcec0c10bbea4171e7406
Kumilachew-g <kumilachew1212@gmail.com>
2023-07-28 13:56:54 +0300

    Add paper component from mui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Paper elevation={3}>
+      <h1 style={{ margin: '0' }}>Header</h1>
+    </Paper>
+  );
 }
 
 export default Header;
