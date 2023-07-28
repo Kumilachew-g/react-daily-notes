151d467211195b2ddd09cb924257552df59c7aee
Kumilachew-g <kumilachew1212@gmail.com>
2023-07-28 14:12:49 +0300

    Import fonts cdn and add styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,15 @@ import Paper from '@material-ui/core/Paper';
 
 function Header() {
   return (
-    <Paper elevation={3}>
-      <h1 style={{ margin: '0' }}>Header</h1>
+    <Paper style={{ padding: '5px', display: 'flex' }} square elevation={3}>
+      <img
+        style={{ width: '60px' }}
+        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYNH9kZSS3ypHqRs37553_d5wTXdAMMq6kvA&usqp=CAU'
+        alt=''
+      />
+      <h1 style={{ margin: '0', fontFamily: 'cursive', fontWeight: '100' }}>
+        Daily Notes
+      </h1>
     </Paper>
   );
 }
